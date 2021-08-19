eca8ba11f8fc74dcf6fab8192ed817303cc36856
YongsulLee <sadghnmb04@gmail.com>
2021-08-19 10:07:37 +0900

    Build settings with eslint and prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line import/no-named-as-default
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
